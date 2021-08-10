7993b928deac80171a2499495a7f0838c70633e4
April211 <wuguangkai2018@outlook.com>
2021-08-10 09:40:14 +0800

    选择当前最佳模型进行转换

--- ev_sdk/model/convert_openvino.sh
+++ ev_sdk/model/convert_openvino.sh
@@ -1,5 +1,4 @@
 source /opt/intel/openvino/deployment_tools/model_optimizer/venv/bin/activate
-cd /project/train/models/
-/opt/intel/openvino/deployment_tools/model_optimizer/mo_onnx.py --input_model /project/train/models/Yolov3_99.onnx --output_dir /project/ev_sdk/model --model_name Yolov3 --batch 1
+/opt/intel/openvino/deployment_tools/model_optimizer/mo_onnx.py --input_model /project/train/models/Yolov3_55.onnx --output_dir /project/ev_sdk/model --model_name Yolov3 --batch 1
 # /usr/local/ev_sdk/model/
 # bash /project/ev_sdk/model/convert_openvino.sh
